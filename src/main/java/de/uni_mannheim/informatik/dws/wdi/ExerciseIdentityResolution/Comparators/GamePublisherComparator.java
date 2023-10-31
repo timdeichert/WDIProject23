401c808565dd558877884adfaaddaf11a97a225d
KokTeng00 <ngkokteng00@gmail.com>
2023-10-31 22:02:09 +0100

    Platform similarity (Monge Elkan) still figuring the embedding similarity measurement

---
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/Comparators/GamePublisherComparator.java
@@ -0,0 +1,52 @@
+package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Comparators;
+
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Game;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.comparators.Comparator;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.comparators.ComparatorLogger;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.similarity.string.JaroWinklerSimilarity;
+
+
+
+public class GamePublisherComparator implements Comparator<Game, Attribute> {
+
+    private static final long serialVersionUID = 1L;
+    private JaroWinklerSimilarity sim = new JaroWinklerSimilarity(); // Use Jaro-Winkler similarity
+
+    private ComparatorLogger comparisonLog;
+
+    @Override
+    public double compare(
+            Game record1,
+            Game record2,
+            Correspondence<Attribute, Matchable> schemaCorrespondences) {
+
+        String publisher1 = record1.getPublisher();
+        String publisher2 = record2.getPublisher();
+
+        double similarity = sim.calculate(publisher1, publisher2); // Use Jaro-Winkler similarity
+
+        if (this.comparisonLog != null) {
+            this.comparisonLog.setComparatorName(getClass().getName());
+
+            this.comparisonLog.setRecord1Value(publisher1);
+            this.comparisonLog.setRecord2Value(publisher2);
+
+            this.comparisonLog.setSimilarity(Double.toString(similarity));
+        }
+        return similarity;
+    }
+
+    @Override
+    public ComparatorLogger getComparisonLog() {
+        return this.comparisonLog;
+    }
+
+    @Override
+    public void setComparisonLog(ComparatorLogger comparatorLog) {
+        this.comparisonLog = comparatorLog;
+    }
+
+}
