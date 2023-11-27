f38fbf51dbecbd49a3e9d2febb8a64db30e05ca6
mehmetegekacar <118284122+mehmetegekacar@users.noreply.github.com>
2023-11-27 17:37:34 +0100

    Add files via upload

---
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/evaluation/EuSalesEvaluation.java
@@ -0,0 +1,26 @@
+package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.evaluation;
+
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Game;
+import de.uni_mannheim.informatik.dws.winter.datafusion.EvaluationRule;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+
+public class EuSalesEvaluation extends EvaluationRule <Game, Attribute> {
+   
+    @Override
+    public boolean isEqual(Game record1, Game record2, Attribute schemaElement) {
+        if (record1.getEU_Sales() == null && record2.getEU_Sales() == null) {
+            return true;
+        } else if (record1.getEU_Sales() == null ^ record2.getEU_Sales() == null) {
+            return false;
+        } else {
+            return record1.getEU_Sales().equals(record2.getEU_Sales());
+        }
+    }
+
+    @Override
+    public boolean isEqual(Game record1, Game record2, Correspondence<Attribute, Matchable> schemaCorrespondence) {
+        return isEqual(record1, record2, (Attribute) null);
+    }
+}
