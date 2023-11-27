614638ee6e7cb28b3dc7a18d4e9fa94d7c5ada5e
mehmetegekacar <118284122+mehmetegekacar@users.noreply.github.com>
2023-11-27 17:30:12 +0100

    Update EuSalesFuserAverage.java

--- src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/Fuser/EuSalesFuserAverage.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/Fuser/EuSalesFuserAverage.java
@@ -1,40 +1,26 @@
-// package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.fusers;
+package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.evaluation;
 
-// import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Game;
-// import de.uni_mannheim.informatik.dws.winter.datafusion.AttributeValueFuser;
-// import de.uni_mannheim.informatik.dws.winter.datafusion.conflictresolution.numeric.Average;
-// import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
-// import de.uni_mannheim.informatik.dws.winter.model.FusedValue;
-// import de.uni_mannheim.informatik.dws.winter.model.Matchable;
-// import de.uni_mannheim.informatik.dws.winter.model.RecordGroup;
-// import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Game;
+import de.uni_mannheim.informatik.dws.winter.datafusion.EvaluationRule;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
 
-// /**
-//  * {@link AttributeValueFuser} for the EU sales of {@link Game}s, calculating the average.
-//  * 
-//  * @author Your Name
-//  */
-// public class EuSalesFuserAverage extends AttributeValueFuser<Float, Game, Game> {
+public class EuSalesEvaluation extends EvaluationRule <Game, Attribute> {
+   
+    @Override
+    public boolean isEqual(Game record1, Game record2, Attribute schemaElement) {
+        if (record1.getEU_Sales() == null && record2.getEU_Sales() == null) {
+            return true;
+        } else if (record1.getEU_Sales() == null ^ record2.getEU_Sales() == null) {
+            return false;
+        } else {
+            return record1.getEU_Sales().equals(record2.getEU_Sales());
+        }
+    }
 
-//     public EuSalesFuserAverage() {
-//         super(new Average<>());
-//     }
-
-//     @Override
-//     public boolean hasValue(Game record, Correspondence<Game, Matchable> correspondence) {
-//         return record.getEU_Sales() != null;
-//     }
-
-//     @Override
-//     public Float getValue(Game record, Correspondence<Game, Matchable> correspondence) {
-//         return record.getEU_Sales();
-//     }
-
-//     @Override
-//     public void fuse(RecordGroup<Game, Game> group, Game fusedRecord,
-//                      Processable<Correspondence<Game, Matchable>> schemaCorrespondences, Game schemaElement) {
-//         FusedValue<Float, Game, Game> fused = getFusedValue(group, schemaCorrespondences, schemaElement);
-//         fusedRecord.setEU_Sales(fused.getValue());
-//         fusedRecord.setAttributeProvenance(Game.EU_SALES, fused.getOriginalIds());
-//     }
-// }
+    @Override
+    public boolean isEqual(Game record1, Game record2, Correspondence<Attribute, Matchable> schemaCorrespondence) {
+        return isEqual(record1, record2, (Attribute) null);
+    }
+}
